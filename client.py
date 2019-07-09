e28af198acaf36ebb5d10228c405f2532980471a
Wade Sherman <wade.sherman@rivdata.com>
2019-07-09 15:44:58 -0700

    move secrets to dotenv file

--- client.py
+++ client.py
@@ -1,10 +1,14 @@
 import paho.mqtt.client as mqtt
 import random
-
+import os
 from time import sleep
+from dotenv import load_dotenv
+
 from garagedoor import GarageDoor
 from gd_io import IO
 from mqttObserver import MqttObserver
+
+load_dotenv(override=True)
 
 def on_message(client, userdata, message):
     requested_state = str(message.payload.decode("utf-8"))
@@ -19,16 +23,16 @@ def on_message(client, userdata, message):
         gd.toggle()
 
 def on_connect(client, userdata, flags, rc):
-    mqttc.subscribe("garage/door/status/set")
+    mqttc.subscribe(os.getenv("COMMAND_TOPIC"))
 
 mqttc = mqtt.Client("MQTT_Client")
 mqttc.on_connect=on_connect
 mqttc.on_message=on_message
-mqttc.connect("mq.casadeoso.com")
+mqttc.connect(os.getenv("MQ_BROKER"))
 mqttc.loop_start()
 
 gd = GarageDoor(IO())
-gd.addObserver(MqttObserver(mqttc, "garage/door/status"))
+gd.addObserver(MqttObserver(mqttc, os.getenv("STATE_TOPIC")))
 
 def main():
     while True:
